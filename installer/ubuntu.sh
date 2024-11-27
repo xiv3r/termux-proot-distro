2297b5daf236f7362a4418b55ccd816fef3f8a6b
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 07:38:57 +0800

    Update ubuntu.sh

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="Ubuntu"
 TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/ubuntu-jammy-aarch64-pd-v4.11.0.tar.xz"
-TARBALL_SHA256['aarch64']="a8883244a7031559a2bd8dc16b7d8afc947930b611819d8a28a09545097a6ba5"
+TARBALL_SHA256['aarch64']="caddd5b6d4dc48fd028e369a9ecb101f96e01ad3957b46e77f637252612ec628"
 distro_setup() {
         # Configure en_US.UTF-8 locale.
         sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
