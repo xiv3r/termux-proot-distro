1c2939ae46839173ba733df92d366921fd454628
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 07:45:18 +0800

    Update kali-full.sh

--- installer/kali-full.sh
+++ installer/kali-full.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="kali-full"
 TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kali-nethunter-rootfs-full-arm64.tar.xz"
-TARBALL_SHA256['aarch64']=""
+TARBALL_SHA256['aarch64']="f213bb0914527256bbd2b3f5a1852d09f65473e832be101fdd3f0f01fec32f85"
 distro_setup() {
         # Configure en_US.UTF-8 locale.
         sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
