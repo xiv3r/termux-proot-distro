b2b702a0cf16d1310f7ce225e52c8aa69d583848
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 17:55:23 +0800

    Create kali-nano.sh

---
+++ installer/kali-nano.sh
@@ -0,0 +1,7 @@
+TARBALL_URL['aarch64']=""
+TARBALL_SHA256['aarch64']=""
+distro_setup() {
+        # Configure en_US.UTF-8 locale.
+        sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
+        run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+}
