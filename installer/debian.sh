c655084e47672d6056fe40efafaa79b8b85171d1
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 18:30:24 +0800

    Create debian.sh

---
+++ installer/debian.sh
@@ -0,0 +1,11 @@
+DISTRO_NAME="Debian (bookworm)"
+DISTRO_COMMENT="Stable release."
+
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v4.7.0/debian-bookworm-aarch64-pd-v4.7.0.tar.xz"
+TARBALL_SHA256['aarch64']="4baa32280cc70b67e2c650777c1d974349f0cdf23afaabc305ad3bc6182b8df8"
+
+distro_setup() {
+        # Configure en_US.UTF-8 locale.
+        sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
+        run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+}
