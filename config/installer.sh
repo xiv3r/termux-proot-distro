db49e370206115b051b28975bc2a2a364f54589f
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 18:20:32 +0800

    Update installer.sh

--- config/installer.sh
+++ config/installer.sh
@@ -2,7 +2,13 @@
 
 apt install -y proot-distro 
 ###
-wget -O $PREFIX/etc/proot-distro/kali-lite.sh https://raw.githubusercontent.com/xiv3r/termux-proot-distro/refs/heads/main/installer/kali.sh
+wget -O $PREFIX/etc/proot-distro/kali-lite.sh https://raw.githubusercontent.com/xiv3r/termux-proot-distro/refs/heads/main/installer/kali-lite.sh
+###
+wget -O $PREFIX/etc/proot-distro/kali-nano.sh https://raw.githubusercontent.com/xiv3r/termux-proot-distro/refs/heads/main/installer/kali-nano.sh
+###
+wget -O $PREFIX/etc/proot-distro/kali-minimal.sh https://raw.githubusercontent.com/xiv3r/termux-proot-distro/refs/heads/main/installer/kali-minimal.sh
+###
+wget -O $PREFIX/etc/proot-distro/kali-full.sh https://raw.githubusercontent.com/xiv3r/termux-proot-distro/refs/heads/main/installer/kali-full.sh
 ###
 chmod a+x $PREFIX/etc/proot-distro/*.sh
 ###
