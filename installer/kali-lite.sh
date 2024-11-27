50d272f205741770ec5ea94e5ccd588624f91e8f
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 07:31:08 +0800

    Update kali-lite.sh

--- installer/kali-lite.sh
+++ installer/kali-lite.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="kali-lite"
 TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/kali_rootfs_lite.tar.xz"
-TARBALL_SHA256['aarch64']="4a3bed0ac847eb6404ec9cf5d627c16361dba3ea8dbf33a5bab2974f0836850f"
+TARBALL_SHA256['aarch64']="1dda4379f6714d0e1df958bf811a12004b0b82a8808efc8a93ded5b299cd9059"
 distro_setup() {
         # Configure en_US.UTF-8 locale.
         sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
