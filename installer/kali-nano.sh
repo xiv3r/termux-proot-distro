b11dde269c0729be8b074c4fb2830faac116ee71
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 12:02:16 +0800

    Update kali-nano.sh

--- installer/kali-nano.sh
+++ installer/kali-nano.sh
@@ -1,4 +1,4 @@
 DISTRO_NAME="kali-nano"
 DISTRO_COMMENT="Kali Nano Release"
-TARBALL_URL['aarch64']="https://old.kali.org/nethunter-images/kali-2024.3/rootfs/kali-nethunter-rootfs-nano-arm64.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/kali-rolling_arm64-rootfs-nano.tar.xz"
 TARBALL_SHA256['aarch64']="a1624e4bb2423dee6e9806455dc963a32d6cbbcbe2d4ac008826d0fd7fe1e773"
