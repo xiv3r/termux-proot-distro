575c6674f717221ed8460d0120904e91409229ca
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 08:39:12 +0800

    Update blackarch.sh

--- installer/blackarch.sh
+++ installer/blackarch.sh
@@ -1,5 +1,5 @@
 DISTRO_NAME="BlackArch"
-DISTRO_COMMENT="Stable release"
+DISTRO_COMMENT="Stable Release"
 TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/archlinux-aarch64-pd-v4.6.0.tar.xz"
 TARBALL_SHA256['aarch64']="7e87d551845aedae5a111d1fdcc2f5a69b0805f365244f3fab3fe67cd4114f00"
 
