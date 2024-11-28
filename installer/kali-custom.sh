82031fe6ea0d17d122c9573101b768f50c8aa8a8
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 11:55:19 +0800

    Delete installer/kali-custom.sh

--- installer/kali-custom.sh
+++
@@ -1,4 +0,0 @@
-DISTRO_NAME="kali-minimal-custom"
-DISTRO_COMMENT="Custom Build"
-TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/kali-arm64-minimal.tar.xz"
-TARBALL_SHA256['aarch64']="408dc56550c1df9e36b5fc98aebb45528d4b377deb097a291f7b65bc5a624f7e"
