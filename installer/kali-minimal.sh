f56cd38e7372596fd7b099b7932b557a9673139f
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 11:59:04 +0800

    Update kali-minimal.sh

--- installer/kali-minimal.sh
+++ installer/kali-minimal.sh
@@ -1,4 +1,4 @@
 DISTRO_NAME="kali-minimal"
 DISTRO_COMMENT="Kali Minimal Release"
-TARBALL_URL['aarch64']="https://old.kali.org/nethunter-images/kali-2024.3/rootfs/kali-nethunter-rootfs-minimal-arm64.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/kali-arm64-minimal.tar.xz"
 TARBALL_SHA256['aarch64']="c49fab64df4b427d240c0af078cca4a58ed1db6c66a321d5f3ab479007a64b1f"
