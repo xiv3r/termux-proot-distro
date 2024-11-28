b4ff090e8ba574a50d5d4e64b44cf0dd7a593c45
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 12:25:39 +0800

    Update and rename kali-nano.shs to kali-minimal.sh

--- installer/kali-minimal.sh
+++ installer/kali-minimal.sh
@@ -1,3 +1,3 @@
-DISTRO_NAME="kali-nano"
+DISTRO_NAME="Kali-Minimal"
 TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/kali-arm64-minimal.tar.xz"
 TARBALL_SHA256['aarch64']="408dc56550c1df9e36b5fc98aebb45528d4b377deb097a291f7b65bc5a624f7e"
