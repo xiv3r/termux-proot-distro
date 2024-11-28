585de069f599983534f82af3d6d42a958331aae8
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 09:57:12 +0800

    Update kali-minimal.sh

--- installer/kali-minimal.sh
+++ installer/kali-minimal.sh
@@ -1,7 +1,7 @@
 DISTRO_NAME="kali-minimal"
 DISTRO_COMMENT="Kali Minimal Release"
 TARBALL_URL['aarch64']="https://old.kali.org/nethunter-images/kali-2024.3/rootfs/kali-nethunter-rootfs-minimal-arm64.tar.xz"
-TARBALL_SHA256['aarch64']=""
+TARBALL_SHA256['aarch64']="0560e294fef5fdbadf077bdd609166c94f68f4be72429ba366b4fe4146694dee"
 distro_setup() {
         # Configure en_US.UTF-8 locale.
         sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
