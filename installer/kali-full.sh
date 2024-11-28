1617fd0e0bfe24ba29cd473c8ace66cdc9bdf38e
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-28 11:55:36 +0800

    Delete installer/kali-full.sh

--- installer/kali-full.sh
+++
@@ -1,4 +0,0 @@
-DISTRO_NAME="kali-full"
-DISTRO_COMMENT="Kali Full Release"
-TARBALL_URL['aarch64']="https://old.kali.org/nethunter-images/kali-2024.3/rootfs/kali-nethunter-rootfs-full-arm64.tar.xz"
-TARBALL_SHA256['aarch64']="f213bb0914527256bbd2b3f5a1852d09f65473e832be101fdd3f0f01fec32f85"
