198ffc87334405a086165bce04bc4b20441a0b8a
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-29 12:44:56 +0800

    Create kali-deb.sh

---
+++ installer/kali-deb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+###
+wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/proot-distro-kali-nethunter/refs/heads/main/bash.bashrc
+###
+apt update && apt install curl gnupg -y
+###
+wget -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc https://archive.kali.org/archive-key.asc
+###
+curl -fsSL https://archive.kali.org/archive-key.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/kali-archive-keyring.gpg
+###
+cp /etc/apt/trusted.gpg.d/kali-archive-keyring.gpg /usr/share/keyrings/
+###
+echo "
+deb [signed-by="/usr/share/keyrings/kali-archive-keyring.gpg"] https://kali.download/kali kali-rolling main contrib non-free non-free-firmware
+" > /etc/apt/sources.list
+###
+apt update && apt full-upgrade -y
+###
+apt --fix-broken install
+###
+dpkg --configure -a
+###
+source /root/.bashrc
