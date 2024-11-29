f424202a9686569dee7a62bf8b124817d1aa9c63
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-29 10:35:24 +0800

    Update kali-minimal.sh

--- installer/kali-minimal.sh
+++ installer/kali-minimal.sh
@@ -1,3 +1,8 @@
 DISTRO_NAME="Kali-Minimal"
 TARBALL_URL['aarch64']="https://github.com/xiv3r/termux-proot-distro/releases/download/rootfs/kali-arm64-minimal.tar.xz"
 TARBALL_SHA256['aarch64']="408dc56550c1df9e36b5fc98aebb45528d4b377deb097a291f7b65bc5a624f7e"
+distro_setup() {
+
+        sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
+        run_proot_cmd locale-gen
+}
